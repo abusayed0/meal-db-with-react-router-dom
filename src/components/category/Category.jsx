e3327f0b45fd7700466b99561be601f4ee1fdb77
abusayed0 <abusayed48876@gmail.com>
2023-09-23 00:31:34 +0600

    active '/Beef' route when user is on '/' (home) route

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,10 +1,11 @@
-import { NavLink} from "react-router-dom";
+import { NavLink, useLocation} from "react-router-dom";
 import PropTypes from 'prop-types';
 const Category = ({category}) => {
-   
+const {strCategory} = category;
+const location = useLocation();
     return (
-        <NavLink to={`/${category.strCategory}`}  className={({isActive})=> isActive ? "text-xl font-medium bg-red-600 px-4 py-2" : "text-xl font-medium border 6order-red-800 px-4 py-2"}>
-           {category.strCategory}
+        <NavLink to={`/${strCategory}`}  className={({isActive})=> isActive || (location.pathname === '/' && strCategory === "Beef") ? "text-xl font-medium bg-red-600 px-4 py-2" : "text-xl font-medium border 6order-red-800 px-4 py-2"}>
+           {strCategory}
         </NavLink>
     );
 };
